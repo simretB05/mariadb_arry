a302bf2d3cefe38b7182a3ad023352e5dba18d0b
simretB05 <happysimret@gmail.com>
2023-07-03 00:11:49 -0600

    added backup for a db

---
+++ mariadb_programmingbackup.sql
@@ -0,0 +1,88 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mariadb_array
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cool_nums`
+--
+
+DROP TABLE IF EXISTS `cool_nums`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cool_nums` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `num` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cool_nums`
+--
+
+LOCK TABLES `cool_nums` WRITE;
+/*!40000 ALTER TABLE `cool_nums` DISABLE KEYS */;
+INSERT INTO `cool_nums` VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,1),(8,-652),(9,3),(10,4),(11,59987),(12,666),(13,1),(14,-652),(15,3),(16,4),(17,59987),(18,1),(19,-652),(20,3),(21,4),(22,59987),(23,1),(24,-652),(25,3),(26,4),(27,1),(28,-652),(29,3),(30,4);
+/*!40000 ALTER TABLE `cool_nums` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mariadb_array'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `insert_num_from_array` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_num_from_array`(num_array_input varchar(500))
+    MODIFIES SQL DATA
+begin
+	declare single_num varchar(20) default '';
+    declare rows_inserted int default 0;
+    set num_array_input = replace(num_array_input, '[', '');
+    set num_array_input = replace(num_array_input, ']', '');
+    while length(num_array_input) > 0 do
+          set single_num =
+             trim(substring_index(num_array_input, ',', 1));
+           set num_array_input =
+                     trim(substring(num_array_input, length(single_num) + 2));
+    insert into cool_nums (num) values(single_num);
+           set rows_inserted = rows_inserted + 1;
+     end while;
+     select rows_inserted;
+     commit;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-03  0:09:53
